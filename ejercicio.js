519a5f8e7d94118edcd9cc269f2ef2b67a37e8e1
Andres Velasco <Avelasco3@cuc.edu.co>
2026-03-09 12:15:50 -0500

    Se agrego la entrada para cada espacio de la nota

---
+++ ejercicio.js
@@ -0,0 +1,7 @@
+// Sistema de Promedio de Notas con decimales
+
+// Ingreso de notas
+let nota1 = parseFloat(prompt("Ingrese la primera nota (puede ser decimal):"));
+let nota2 = parseFloat(prompt("Ingrese la segunda nota (puede ser decimal):"));
+let nota3 = parseFloat(prompt("Ingrese la tercera nota (puede ser decimal):"));
+
