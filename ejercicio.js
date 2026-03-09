c9a80e13b5c4c00bb27e121433748b594ca57d33
Andres Velasco <Avelasco3@cuc.edu.co>
2026-03-09 12:18:41 -0500

    Se agrego el comando para mostrar el promedio del estudiante

--- ejercicio.js
+++ ejercicio.js
@@ -8,3 +8,6 @@ let nota3 = parseFloat(prompt("Ingrese la tercera nota (puede ser decimal):"));
 // Cálculo del promedio
 let promedio = (nota1 + nota2 + nota3) / 3;
 
+// Mostrar promedio con 2 decimales
+console.log("El promedio es: " + promedio.toFixed(2));
+
