69d507f91fe3137c1696e704d16fcf590dad1c99
Andres Velasco <Avelasco3@cuc.edu.co>
2026-03-09 12:16:58 -0500

    Se agrego el ejercicio para determinar el promedio de la nota

--- ejercicio.js
+++ ejercicio.js
@@ -5,3 +5,6 @@ let nota1 = parseFloat(prompt("Ingrese la primera nota (puede ser decimal):"));
 let nota2 = parseFloat(prompt("Ingrese la segunda nota (puede ser decimal):"));
 let nota3 = parseFloat(prompt("Ingrese la tercera nota (puede ser decimal):"));
 
+// Cálculo del promedio
+let promedio = (nota1 + nota2 + nota3) / 3;
+
